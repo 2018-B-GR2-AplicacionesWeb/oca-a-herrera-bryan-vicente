b6589211a28e58aaeb57159093734500f24b91ca
EPS-PC\Bryan <bryan.ocana@epn.edu.ec>
2018-11-17 12:20:48 -0500

    Cambio de prueba

--- Mini-Proyecto/Delete-Items.js
+++ Mini-Proyecto/Delete-Items.js
@@ -1,7 +1,8 @@
-var filepath = "C:/directorio-cualquiera/archivo-existente.txt";
+var filepath = "Comics.txt";
 fs.exists(filepath, function(exists) {
       if(exists) {
             // El archivo existe, borrar
+            
             fs.unlink(filepath,function(err){
                   if(err){
                       alert("Ha ocurrido un error al eliminar el archivo"+ err.message);
@@ -11,6 +12,6 @@ fs.exists(filepath, function(exists) {
               console.log("Archivo satisfactoriamente eliminado");
             });
       } else {
-           alert("El archivo no existe, por lo tanto no se puede borrar");
+        console.log("El archivo no existe, por lo tanto no se puede borrar");
       }
 });
