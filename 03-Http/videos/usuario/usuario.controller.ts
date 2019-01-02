e8fbea1fc27b09783fa2d2f9cd55f3674eb6e867
bryancdn969 <bryan.cdn.20@gmail.com>
2019-01-02 15:05:48 -0500

    inicio bdd

--- 03-Http/videos/usuario/usuario.controller.ts
+++ 03-Http/videos/usuario/usuario.controller.ts
@@ -1,86 +1,15 @@
-import {
-    Get,
-    Controller,
-    Request,
-    Response,
-    Headers,
-    HttpCode,
-    HttpException,
-    Query,
-    Param,
-    Res,
-    Post, Body
-} from '@nestjs/common';
-import {AppService} from './app.service';
-import {Observable, of} from "rxjs";
-import {Usuario, UsuarioService} from "../usuario/usuario.service";
+// usuario.controller.ts
+import {Body, Controller, Get, Param, Post, Query, Res} from "@nestjs/common";
+import {Usuario, UsuarioService} from "./usuario.service";
 
-// http://192.168.1.2:3000/Usuario/saludar     METODO -> GET
-// http://192.168.1.2:3000/Usuario/salir   METODO -> POST
-// http://192.168.1.2:3000/Usuario/registrar METODO -> PUT
-// http://192.168.1.2:3000/Usuario/borrar METODO -> DELETE
-// http://192.168.1.2:3000/Notas
-
-
-// Decorador -> FUNCION
-// SE EJECUTA ANTES DE ALGO
-@Controller('Usuario') // Decoradores!
-export class AppController {
-
-    // CONSTRUCTOR NO ES UN CONSTRUCTOR NORMAL!!!
+@Controller('Usuario')
+export class UsuarioController {
 
     constructor(
         private readonly _usuarioService: UsuarioService,
-        // private readonly _appService:AppService,
     ) {
 
     }
-
-
-    @Get('saludar')
-    saludar(
-        @Query() queryParams,
-        @Query('nombre') nombre,
-        @Headers('seguridad') seguridad,
-    ): string { // metodo!
-        return nombre;
-    }
-
-    // /Usuario/segmentoUno/12/segmentoDos
-    @Get('segmentoUno/:idUsuario/segmentoDos')
-    ruta(
-        @Param() todosParametrosRuta,
-        @Param('idUsuario') idUsuario,
-    ): string { // metodo!
-        return idUsuario;
-    }
-
-
-    @Get('despedirse')
-    @HttpCode(201)
-    despedirse(): Promise<string> {
-        return new Promise<string>(
-            (resolve, reject) => {
-
-                throw new HttpException({
-                        mensaje: 'Error en despedirse',
-                    },
-                    400);
-            }
-        );
-    }
-
-    @Get('tomar')
-    @HttpCode(201)
-    tomar(): string { // metodo!
-        return 'Estoy borracho';
-    }
-
-    @Get('saludarObservable')
-    saludarObservable(): Observable<string> { // metodo!
-        return of('Hola mundo');
-    }
-
 
     @Get('inicio')
     inicio(
@@ -191,5 +120,8 @@ export class AppController {
 
         response.redirect('/Usuario/inicio' + parametrosConsulta)
     }
-
 }
+
+
+
+
